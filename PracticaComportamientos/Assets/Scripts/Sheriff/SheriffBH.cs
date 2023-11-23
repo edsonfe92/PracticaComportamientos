95bde9cbcc28ee4450e2adf2c657b6c261b819af
edsonfe92 <ev.fernandez.2019@alumnos.urjc.es>
2023-11-23 20:20:35 +0100

    bt pt1

--- PracticaComportamientos/Assets/Scripts/Sheriff/SheriffBH.cs
+++ PracticaComportamientos/Assets/Scripts/Sheriff/SheriffBH.cs
@@ -1,14 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
 using BehaviourAPI.BehaviourTrees;
 using BehaviourAPI.Core;
 using BehaviourAPI.Core.Actions;
+using BehaviourAPI.UnityToolkit;
 using UnityEngine;
 
 public class SheriffBH : MonoBehaviour
-{    
-    Transform[] locations;
-        private void Start() 
-    {                
-        BehaviourTree bt = new BehaviourTree();
+{      
+    public List<Transform> pivots = new List<Transform>();
+    BehaviourTree bt = new BehaviourTree();                 
+    private void Start() 
+    {                    
+        WalkAction toBank = new WalkAction(pivots[(int)Location.BANCO].position);
+        WalkAction toTavern = new WalkAction(pivots[(int)Location.TAVERNA].position);
+        WalkAction toMines = new WalkAction(pivots[(int)Location.MINA].position);
+
+        LeafNode patrolToBank = bt.CreateLeafNode("patrolToBank", toBank);
+        LeafNode patrolToTavern = bt.CreateLeafNode("patrolToTavern", toTavern);
+        LeafNode patrolToMines = bt.CreateLeafNode("patrolToMines", toMines);
+        
+        bt.SetRootNode(patrolToBank);
+
+        bt.Start();
+    }
+    private void Update() 
+    {
+        bt.Update();
     }
 }
     
