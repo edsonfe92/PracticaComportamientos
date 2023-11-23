8cb26072408133e6fe1b537c8895c19c05510492
Allog0 <allogo.2001@gmail.com>
2023-11-23 20:22:02 +0100

    a

--- PracticaComportamientos/Assets/Scripts/ForajidoBT.cs
+++ PracticaComportamientos/Assets/Scripts/ForajidoBT.cs
@@ -9,20 +9,36 @@ using UnityEngine.AI;
 
 public class ForajidoBT : MonoBehaviour
 {
-    BehaviourTree forajidobt = new BehaviourTree();
+    //BehaviourTree forajidobt = new BehaviourTree();
 
     public Transform[] pivotArray;
 
+    BehaviourTree forajidobt = new BehaviourTree();
+    public List<Transform> PivotTransforms = new List<Transform>();
+
     protected override BehaviourGraph CreateGraph()
     {
-        var walkToMineAction = new WalkAction(MinePos);
+        var BankPos = PivotTransforms[(int)Location.BANCO].position;
+        var MinePos = PivotTransforms[(int)Location.BANCO].position;
+        var TavernPos = PivotTransforms[(int)Location.BANCO].position;
+
         var walkToBankAction = new WalkAction(BankPos);
+        var walkToMineAction = new WalkAction(MinePos);       
         var walkToTavernAction = new WalkAction(TavernPos);
 
         var walkToMine = forajidobt.CreateLeafNode("walkToMine", walkToMineAction); // Camina a la mina
         var walkToBank = forajidobt.CreateLeafNode("walkToBank", walkToBankAction); // Camina al banco
-        var walkToTavern = forajidobt.CreateLeafNode("walkToTavern", walkToTavernAction); // Camina a la taberna
+        var walkToTavern = forajidobt.CreateLeafNode("walkToTavern", walkToTavernAction); // Camina a la taberna        
     }
 
+    private void Start()
+    {
+        forajidobt.Start();
+    }
+
+    private void Update()
+    {
+        forajidobt.Update();
+    }
 
 }
