b151e15e45e624eff9db016144a11ef50f5f960c
Allog0 <allogo.2001@gmail.com>
2023-12-05 17:45:37 +0100

    Forajido y entradas

--- PracticaComportamientos/Assets/Scripts/ForajidoBT.cs
+++ PracticaComportamientos/Assets/Scripts/ForajidoBT.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using BehaviourAPI.BehaviourTrees;
 using BehaviourAPI.Core;
 using BehaviourAPI.Core.Actions;
-using BehaviourAPI.UnityToolkit.GUIDesigner.Runtime;
+using BehaviourAPI.UnityToolkit;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -16,24 +16,75 @@ public class ForajidoBT : MonoBehaviour
     BehaviourTree forajidobt = new BehaviourTree();
     public List<Transform> PivotTransforms = new List<Transform>();
 
-    protected override BehaviourGraph CreateGraph()
-    {
-        var BankPos = PivotTransforms[(int)Location.BANCO].position;
-        var MinePos = PivotTransforms[(int)Location.BANCO].position;
-        var TavernPos = PivotTransforms[(int)Location.BANCO].position;
+    public float tiempoDeEspera = 5.0f; // Tiempo en segundos antes de que el forajido aparezca nuevamente
 
-        var walkToBankAction = new WalkAction(BankPos);
-        var walkToMineAction = new WalkAction(MinePos);       
-        var walkToTavernAction = new WalkAction(TavernPos);
+    private float tiempoPasado = 0.0f;
 
-        var walkToMine = forajidobt.CreateLeafNode("walkToMine", walkToMineAction); // Camina a la mina
-        var walkToBank = forajidobt.CreateLeafNode("walkToBank", walkToBankAction); // Camina al banco
-        var walkToTavern = forajidobt.CreateLeafNode("walkToTavern", walkToTavernAction); // Camina a la taberna        
-    }
 
     private void Start()
     {
+        var BankPos = PivotTransforms[(int)Location.BANCO].position;
+        var Entrada1Pos = PivotTransforms[(int)Location.ENTRADA1].position;
+        var Entrada2Pos = PivotTransforms[(int)Location.ENTRADA2].position;
+        var Entrada3Pos = PivotTransforms[(int)Location.ENTRADA3].position;
+        //var MinePos = PivotTransforms[(int)Location.BANCO].position;
+        //var TavernPos = PivotTransforms[(int)Location.BANCO].position;
+
+        var walkToBankAction = new WalkAction(BankPos);
+        var walkToEscapeAction = new WalkAction(Entrada1Pos);
+        //var walkToMineAction = new WalkAction(MinePos);
+        //var walkToTavernAction = new WalkAction(TavernPos);
+
+        var walkToBank = forajidobt.CreateLeafNode("walkToBank", walkToBankAction); // Camina al banco
+        var stealAction = new FunctionalAction(EnterBank);
+        var walkToEscape = forajidobt.CreateLeafNode("walkToEscape", walkToEscapeAction); // Camina al banco
+        var detainAction = new FunctionalAction(Detained);
+        var Duel = new FunctionalAction(EnterBank, () => Status.Success);
+        //var walkToMine = forajidobt.CreateLeafNode("walkToMine", walkToMineAction); // Camina a la mina
+        //var walkToTavern = forajidobt.CreateLeafNode("walkToTavern", walkToTavernAction); // Camina a la taberna  
+
         forajidobt.Start();
+    }
+
+    private void EnterBank()
+    {
+        DesaparecerForajido();
+        //Tiene que pasar tiempo o algo aquí
+        if (!gameObject.activeSelf)
+        {
+            tiempoPasado += Time.deltaTime;
+
+            // Verificar si ha pasado el tiempo de espera
+            if (tiempoPasado >= tiempoDeEspera)
+            {
+                // Ha pasado el tiempo de espera, hacer que la persona aparezca nuevamente
+                AparecerForajido();
+            }
+        }
+    }
+
+    private void Escape()
+    {
+        //Logica en la que el forajido sale y corre a ala salida
+    }
+    private void Detained()
+    {
+        
+    }
+
+    private void DesaparecerForajido()
+    {
+        // Desactivar el GameObject del forajido
+        gameObject.SetActive(false);
+    }
+
+    private void AparecerForajido()
+    {
+        // Activar el GameObject de la persona
+        gameObject.SetActive(true);
+
+        // Reiniciar el tiempo pasado
+        tiempoPasado = 0.0f;
     }
 
     private void Update()
--- PracticaComportamientos/Assets/Scripts/LocationsEnum.cs
+++ PracticaComportamientos/Assets/Scripts/LocationsEnum.cs
@@ -3,6 +3,9 @@ public enum Location
 {
     BANCO = 0,
     TAVERNA = 1,
-    MINA = 2
+    MINA = 2,
+    ENTRADA1 = 3,
+    ENTRADA2 = 4,
+    ENTRADA3 = 5
 
 }
