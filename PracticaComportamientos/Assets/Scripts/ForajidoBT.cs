76808217e821b542ced9f446f5f6fec6492855a7
Allog0 <allogo.2001@gmail.com>
2023-12-05 18:56:38 +0100

    compila sin errores xd

--- PracticaComportamientos/Assets/Scripts/ForajidoBT.cs
+++ PracticaComportamientos/Assets/Scripts/ForajidoBT.cs
@@ -7,30 +7,38 @@ using BehaviourAPI.UnityToolkit;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ForajidoBT : MonoBehaviour
+public class ForajidoBT : BehaviourRunner
 {
     //BehaviourTree forajidobt = new BehaviourTree();
 
-    public Transform[] pivotArray;
+    //public Transform[] pivotArray;
 
-    BehaviourTree forajidobt = new BehaviourTree();
+    
     public List<Transform> PivotTransforms = new List<Transform>();
 
     public float tiempoDeEspera = 5.0f; // Tiempo en segundos antes de que el forajido aparezca nuevamente
 
     private float tiempoPasado = 0.0f;
 
-
-    private void Start()
+    protected override void Init()
     {
+        base.Init();
+    }
+
+    protected override BehaviourGraph CreateGraph()
+    {
+        BehaviourTree forajidobt = new BehaviourTree();
+
         var BankPos = PivotTransforms[(int)Location.BANCO].position;
+        Debug.Log(PivotTransforms[(int)Location.BANCO].position);
+
         var Entrada1Pos = PivotTransforms[(int)Location.ENTRADA1].position;
         var Entrada2Pos = PivotTransforms[(int)Location.ENTRADA2].position;
         var Entrada3Pos = PivotTransforms[(int)Location.ENTRADA3].position;
         //var MinePos = PivotTransforms[(int)Location.BANCO].position;
         //var TavernPos = PivotTransforms[(int)Location.BANCO].position;
 
-        var walkToBankAction = new WalkAction(BankPos);
+        var walkToBankAction = new WalkAction(BankPos); 
         var walkToEscapeAction = new WalkAction(Entrada1Pos);
         //var walkToMineAction = new WalkAction(MinePos);
         //var walkToTavernAction = new WalkAction(TavernPos);
@@ -41,9 +49,13 @@ public class ForajidoBT : MonoBehaviour
         var detainAction = new FunctionalAction(Detained);
         var Duel = new FunctionalAction(EnterBank, () => Status.Success);
         //var walkToMine = forajidobt.CreateLeafNode("walkToMine", walkToMineAction); // Camina a la mina
-        //var walkToTavern = forajidobt.CreateLeafNode("walkToTavern", walkToTavernAction); // Camina a la taberna  
-
-        forajidobt.Start();
+        //var walkToTavern = forajidobt.CreateLeafNode("walkToTavern", walkToTavernAction); // Camina a la taberna
+        forajidobt.SetRootNode(walkToBank);
+        return forajidobt;
+    }
+    private void Start()
+    {
+          
     }
 
     private void EnterBank()
@@ -75,6 +87,7 @@ public class ForajidoBT : MonoBehaviour
     private void DesaparecerForajido()
     {
         // Desactivar el GameObject del forajido
+        Debug.Log("Desaparesco");
         gameObject.SetActive(false);
     }
 
@@ -89,7 +102,7 @@ public class ForajidoBT : MonoBehaviour
 
     private void Update()
     {
-        forajidobt.Update();
+
     }
 
 }
