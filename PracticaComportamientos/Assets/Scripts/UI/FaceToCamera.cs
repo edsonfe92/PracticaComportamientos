d6c75bf2d21c709f5ad69ca799461b8fda9a541e
edsonfe92 <ev.fernandez.2019@alumnos.urjc.es>
2024-06-03 22:18:33 +0200

    import

---
+++ PracticaComportamientos/Assets/Scripts/UI/FaceToCamera.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceToCamera : MonoBehaviour
+{
+    public Transform cam;
+    private void LateUpdate() 
+    {
+        transform.LookAt(transform.position + cam.forward);    
+    }
+}
